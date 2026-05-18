891117e7729062b0663f4f80c84b82bd0d591475
Ranjeetvelocity13 <ranjeetvelocity13@gmail.com>
2026-05-18 20:17:03 +0530

    Added string methods

---
+++ Day3/String.js
@@ -0,0 +1,23 @@
+console.log("Hello World!");
+let name = "John";
+console.log(name);
+
+let age = 30;
+console.log(age);
+
+let isStudent = true;
+console.log(isStudent);
+
+let hobbies = ["reading", "gaming", "coding"];
+console.log(hobbies);
+
+let person = {
+    name: "Alice",
+    age: 25,
+    isStudent: false
+};
+console.log(person);
+
+function greet(name) {
+    return "Hello, " + name + "!";
+}
